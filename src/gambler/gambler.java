599d3d91b3990869b13103e218150f077451f79b
Dhananjay301996 <gadhavedhananjay123@gmail.com>
2022-06-27 20:30:21 +0530

    Added first uc

---
+++ src/gambler/gambler.java
@@ -0,0 +1,30 @@
+package gambler;
+import java.util.*;
+
+/**
+ * use java.uitil.* package
+ * @author HP
+ *
+ */
+
+public class gambler {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*/
+		 * taking variables stack and bet
+		 * use Random function
+		 * 
+		 */
+		System.out.println("Welcome to Gambler Game Program");
+		int stack = 100;
+		int bet = 1;
+		
+		System.out.println("Total stack of gambler: "+stack);
+		System.out.println("Gambler betting Price: "+bet);
+		
+	
+	}
+
+}
